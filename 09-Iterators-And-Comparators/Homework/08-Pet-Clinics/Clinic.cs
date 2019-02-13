99674971e08ddd7e6479f95a6108ed9ca263e339
KostadinovK <kostadinovk2001@gmail.com>
2019-02-13 10:00:04 +0200

    Solved the homework to lecture 09

---
+++ 09-Iterators-And-Comparators/Homework/08-Pet-Clinics/Clinic.cs
@@ -0,0 +1,107 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+public class Clinic
+{
+    private int index;
+    public string Name { get; set; }
+
+    public Pet[] Rooms { get; set; }
+
+    public Clinic(string name, int rooms)
+    {
+        if (rooms % 2 == 0)
+        {
+            throw new ArgumentException();
+        }
+
+        Name = name;
+        Rooms = new Pet[rooms];
+        index = rooms / 2;
+    
+    }
+
+    public bool Add(Pet pet)
+    {
+        for (int i = 0; i <= index; i++)
+        {
+            if (Rooms[index - i] == null)
+            {
+                Rooms[index - i] = pet;
+                return true;
+            }
+
+            if (Rooms[index + i] == null)
+            {
+                Rooms[index + i] = pet;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public bool Release()
+    {
+        for (int i = 0; i < index; i++)
+        {
+            if (Rooms[index + i] != null)
+            {
+                Rooms[index + i] = null;
+                return true;
+            }
+        }
+
+        for (int i = 0; i < index; i++)
+        {
+            if (Rooms[i] != null)
+            {
+                Rooms[i] = null;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public bool HasEmptyRooms()
+    {
+        return Rooms.Any(x => x == null);
+    }
+
+    public string PrintRoom(int room)
+    {
+        if (Rooms[room] == null)
+        {
+            return "Room empty";
+        }
+
+        return Rooms[room].ToString();
+
+    }
+
+
+    public string Print()
+    {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < Rooms.Length; i++)
+        {
+            if (Rooms[i] == null)
+            {
+                sb.AppendLine("Room empty");
+            }
+            else
+            {
+                sb.AppendLine(Rooms[i].ToString());
+            }
+        }
+
+        return sb.ToString();
+    }
+}
+
---
+++ 09-Iterators-And-Comparators/Homework/08-Pet-Clinics/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        List<Clinic> clinics = new List<Clinic>();
+        List<Pet> pets = new List<Pet>();
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] command = Console.ReadLine().Split().ToArray();
+
+            switch (command[0])
+            {
+                case "Create":
+                    if (command[1] == "Pet")
+                    {
+                        Pet p = new Pet(command[2], int.Parse(command[3]), command[4]);
+                        pets.Add(p);
+                    }else if (command[1] == "Clinic")
+                    {
+                        try
+                        {
+                            Clinic c = new Clinic(command[2], int.Parse(command[3]));
+                            clinics.Add(c);
+                        }
+                        catch (ArgumentException e)
+                        {
+                            Console.WriteLine("Invalid Operation!");
+                        }
+                    }
+
+                    break;
+
+                case "Add":
+
+                    Pet pet = pets.FirstOrDefault(x => x.Name == command[1]);
+                    Clinic clinic = clinics.FirstOrDefault(x => x.Name == command[2]);
+
+                    Console.WriteLine(clinic?.Add(pet));
+
+                    break;
+
+                case "Release":
+
+                    Console.WriteLine(clinics.FirstOrDefault(x => x.Name == command[1])?.Release());
+
+                    break;
+
+                case "HasEmptyRooms":
+
+                    Console.WriteLine(clinics.FirstOrDefault(x => x.Name == command[1])?.HasEmptyRooms());
+                    break;
+
+                case "Print":
+
+                    if (command.Length == 3)
+                    {
+                        Console.WriteLine(clinics.FirstOrDefault(x => x.Name == command[1])?.PrintRoom(int.Parse(command[2]) - 1));
+                    }
+                    else
+                    {
+                        Console.WriteLine(clinics.FirstOrDefault(x => x.Name == command[1])?.Print());
+                    }
+
+                    break;
+            }
+        }
+    }
+}
